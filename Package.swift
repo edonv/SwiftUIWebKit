cbf8138a855ace3a570bafb34c24495572741533
Edon <edon@valdman.works>
2024-01-21 12:50:26 -0600

    Added specific platforms.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftUIWebKit",
+    platforms: [
+        .iOS(.v13),
+        .macCatalyst(.v13),
+        .macOS(.v10_15),
+        .visionOS(.v1)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
