b52289bd15cd8ad4ed7a21f0e85e3860c6810ed6
Edon <edon@valdman.works>
2024-01-25 19:58:52 -0600

    Update WebView.swift
    Made parameters public and added public init.

--- Sources/SwiftUIWebKit/WebView.swift
+++ Sources/SwiftUIWebKit/WebView.swift
@@ -10,14 +10,20 @@ import WebKit
 
 /// A view that displays interactive web content, such as for an in-app browser.
 public struct WebView {
-    @ObservedObject var delegate: WebViewDelegate
+    @ObservedObject
+    public var delegate: WebViewDelegate
     
-    var configuration: WKWebViewConfiguration?
+    public var configuration: WKWebViewConfiguration?
+    
+    public init(delegate: WebViewDelegate, configuration: WKWebViewConfiguration? = nil) {
+        self._delegate = .init(initialValue: delegate)
+        self.configuration = configuration
+    }
     
     /// Returns a Boolean value that indicates whether WebKit natively supports resources with the specified URL scheme.
     /// - Parameter urlScheme: The URL scheme associated with the resource.
     /// - Returns: `true` if WebKit provides native support for the URL scheme, or `false` if it doesn't.
-    static func handlesURLScheme(_ urlScheme: String) -> Bool {
+    public static func handlesURLScheme(_ urlScheme: String) -> Bool {
         WKWebView.handlesURLScheme(urlScheme)
     }
 }
