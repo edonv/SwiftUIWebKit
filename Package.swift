165774abbf761aff79781d992968017c13fda05b
Edon <edon@valdman.works>
2024-03-22 13:31:56 -0500

    Added empty privacy manifest

--- Package.swift
+++ Package.swift
@@ -20,7 +20,10 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(name: "SwiftUIWebKit"),
+        .target(
+            name: "SwiftUIWebKit",
+            resources: [.copy("../PrivacyInfo.xcprivacy")]
+        ),
         .testTarget(
             name: "SwiftUIWebKitTests",
             dependencies: ["SwiftUIWebKit"]),
